4ca38f9ac9b6464c3e5adb25f6e15b6c61e925ba
initial-Dot <graaex@hotmail.com>
2024-04-12 07:09:21 +0800

    update

--- extractor.py
+++ extractor.py
@@ -24,38 +24,44 @@ def readImage(f:BufferedReader, name:str):
     height = readInt(f)
     is_BMP = readInt(f)
     if width > 65535 or height > 65535 or width < 1 or height < 1:
-        print('不是图像文件')
         return False
     if is_BMP == 1:
-        readBmp(f, width, height)
+        name += '.bmp'
+        print(name)
+        if os.path.exists(name):
+            return True
+        # 从这里开始读取bmp格式
+        img = Image.new('RGB', (width, height))
+        for y in range(height):
+            for x in range(width):
+                pal = PAL[readByte(f)]
+                img.putpixel((x, y), (pal.r, pal.g, pal.b))
+    elif is_BMP == 0:
+        name += '.png'
+        print(name)
+        if os.path.exists(name):
+            return True
+        # 从这里开始读取glb自带的图像格式
+        # 跳过一些无用数据 lines
+        f.seek(4 * height, os.SEEK_CUR)
+        # 先按宽高创建全透明的png对象
+        img = Image.new('RGBA', (width, height))
+        img.putalpha(0)
+        while True:
+            # posX:int, posY:int, count:int, colorArr:bytes[count]
+            x = readInt(f)
+            y = readInt(f)
+            count = readInt(f)
+            if x < 0 or x > width or y < 0 or y > height or count == -1:
+                break
+            for i in range(count):
+                pal = PAL[readByte(f)]
+                img.putpixel((x + i, y), (pal.r, pal.g, pal.b, 255))
+    else:
         return False
-    # 从这里开始读取glb自带的图像格式
-    # 跳过一些无用数据 lines
-    f.seek(4 * height, os.SEEK_CUR)
-    # 先按宽高创建全透明的png对象
-    png = Image.new('RGBA', (width, height))
-    png.putalpha(0)
-    while True:
-        # posX:int, posY:int, count:int, colorArr:bytes[count]
-        x = readInt(f)
-        y = readInt(f)
-        count = readInt(f)
-        if x < 0 or x > width or y < 0 or y > height or count == -1:
-            break
-        for i in range(count):
-            if x + i > width:
-                print(x + i, '超出边界', width)
-                continue
-            pal = PAL[readByte(f)]
-            png.putpixel((x + i, y), (pal.r, pal.g, pal.b, 255))
-    print(name + '.png')
-    png.save(name + '.png')
+    img.save(name)
     return True
-
-
-def readBmp(f:BufferedReader, width:int, height:int):
-    print('读取bmp文件')
-
+    
 
 def convert_6bit_to_8bit(value):
     return (value << 2) | (value >> 4)
@@ -94,6 +100,10 @@ while 1:
     with open(prompt, 'rb') as f:
         if not os.path.exists(dirs):
             os.mkdir(dirs)
+            os.mkdir(dirs + '/image')
+            os.mkdir(dirs + '/wav')
+            os.mkdir(dirs + '/midi')
+            os.mkdir(dirs + '/bin')
         header = f.read(8).decode()
         subfile_count = readInt(f)
         reserved_int = readInt(f)
@@ -106,11 +116,32 @@ while 1:
             f_infos.append(sub_file_info(i, file_offset, file_len, re.sub(r'[\x01\\/*?:"<>|\0]', "", file_name)))
         for info in f_infos:
             f.seek(info.offset)
+            if info.len == 0:
+                continue
             # print(info.name)
+            f_name = f'{info.idx}_{info.name}'
             if info.name == 'palette':
                 load_pal(f)
-            elif readImage(f, f'{dirs}/{info.idx}_{info.name}'):
+            elif readImage(f, f'{dirs}/image/{f_name}'):
                 pass
+            else:
+                f.seek(info.offset)
+                try:
+                    sub_header = f.read(4).decode()
+                    print(sub_header)
+                    if sub_header == 'RIFF':
+                        path = f'{dirs}/wav/{f_name}.wav'
+                    elif sub_header == 'MThd':
+                        path = f'{dirs}/midi/{f_name}.mid'
+                    else:
+                        path = f'{dirs}/bin/{f_name}.bin'
+                except UnicodeDecodeError:
+                    path = f'{dirs}/bin/{f_name}.bin'
+                
+                f.seek(info.offset)
+                with open(path, 'wb') as ex_f:
+                    ex_f.write(f.read(info.len))
+                    
             # fb = f.read(info.len)
             # with open(f'{dirs}/{info.name}.bin', 'wb') as extract_f:
             #     extract_f.write(fb)
