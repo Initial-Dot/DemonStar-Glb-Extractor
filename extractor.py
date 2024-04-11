ede17fa410af082a418b017e9971fc4f05252b83
initial-Dot <graaex@hotmail.com>
2024-04-11 21:44:40 +0800

    update

--- extractor.py
+++ extractor.py
@@ -1,6 +1,6 @@
 import os
 import re
-import binascii
+from PIL import Image
 from io import BufferedReader
 
 class sub_file_info:
@@ -12,14 +12,18 @@ class sub_file_info:
     
     
 def readInt(f:BufferedReader):
-    return int.from_bytes(f.read(4), 'little')
+    return int.from_bytes(bytes=f.read(4), byteorder='little', signed=True)
 
 
-def readImage(f:BufferedReader):
+def readByte(f:BufferedReader):
+    return int.from_bytes(f.read(1), 'little')
+
+
+def readImage(f:BufferedReader, name:str):
     width = readInt(f)
     height = readInt(f)
     is_BMP = readInt(f)
-    if width > 65535 or height > 65535 or width < 0 or height < 0:
+    if width > 65535 or height > 65535 or width < 1 or height < 1:
         print('不是图像文件')
         return False
     if is_BMP == 1:
@@ -28,6 +32,9 @@ def readImage(f:BufferedReader):
     # 从这里开始读取glb自带的图像格式
     # 跳过一些无用数据 lines
     f.seek(4 * height, os.SEEK_CUR)
+    # 先按宽高创建全透明的png对象
+    png = Image.new('RGBA', (width, height))
+    png.putalpha(0)
     while True:
         # posX:int, posY:int, count:int, colorArr:bytes[count]
         x = readInt(f)
@@ -35,15 +42,44 @@ def readImage(f:BufferedReader):
         count = readInt(f)
         if x < 0 or x > width or y < 0 or y > height or count == -1:
             break
-        colorBytes = binascii.hexlify(f.read(count)).decode()
-        print(x,y,count, colorBytes)
+        for i in range(count):
+            if x + i > width:
+                print(x + i, '超出边界', width)
+                continue
+            pal = PAL[readByte(f)]
+            png.putpixel((x + i, y), (pal.r, pal.g, pal.b, 255))
+    print(name + '.png')
+    png.save(name + '.png')
     return True
 
 
 def readBmp(f:BufferedReader, width:int, height:int):
     print('读取bmp文件')
+
+
+def convert_6bit_to_8bit(value):
+    return (value << 2) | (value >> 4)
+
+
+class RGBPAL:
+    def __init__(self, r, g, b):
+        self.r = convert_6bit_to_8bit(r)
+        self.g = convert_6bit_to_8bit(g)
+        self.b = convert_6bit_to_8bit(b)
+        
+        
+PAL:list[RGBPAL] = []
+def load_pal(f:BufferedReader):
+    PAL.clear()
+    for i in range(256):
+        PAL.append(RGBPAL(readByte(f), readByte(f), readByte(f)))
     
-    
+
+if os.path.exists('palette'):
+    with open('palette','rb') as pal_f:
+        load_pal(pal_f)
+        print('已载入默认色板')
+        
 if not os.path.exists('extracts'):
     os.mkdir('extracts')    
     
@@ -61,7 +97,7 @@ while 1:
         header = f.read(8).decode()
         subfile_count = readInt(f)
         reserved_int = readInt(f)
-        print(header,subfile_count)   
+        # print(header,subfile_count)   
         f_infos:list[sub_file_info] = []
         for i in range(subfile_count):
             file_offset = readInt(f)
@@ -70,15 +106,13 @@ while 1:
             f_infos.append(sub_file_info(i, file_offset, file_len, re.sub(r'[\x01\\/*?:"<>|\0]', "", file_name)))
         for info in f_infos:
             f.seek(info.offset)
-            print(info.name)
+            # print(info.name)
             if info.name == 'palette':
-                continue
-            if readImage(f):
-                break
+                load_pal(f)
+            elif readImage(f, f'{dirs}/{info.idx}_{info.name}'):
+                pass
             # fb = f.read(info.len)
-            # valid_name = re.sub(r'[\x01\\/*?:"<>|\0]', "", f'{info.idx}_{info.name}.bin')
-            # print(valid_name)
-            # with open(dirs + '\\' + valid_name, 'wb') as extract_f:
+            # with open(f'{dirs}/{info.name}.bin', 'wb') as extract_f:
             #     extract_f.write(fb)
             
             
